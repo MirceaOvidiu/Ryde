0a390681eee29c3fa9c2d65b37797a4797c4e172
Rares-Hampi <hampiraresmarian@gmail.com>
2025-05-15 15:16:21 +0300

    delete all we don't need and make the logic

---
+++ Ryde/src/main/java/com/example/ryde/isp/Statii.java
@@ -0,0 +1,18 @@
+package com.example.ryde.isp;
+
+public enum Statii {
+    Politehnica("Politehnica"),
+    Erorilor("Eroilor"),
+    Grozavesti("Grozavesti"),
+    Izvor("Izvor");
+
+    private final String stationName;
+
+    Statii(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+}
